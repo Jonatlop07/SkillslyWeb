75e4da9d2d1b8908a0bf4731ee6212c9a86abfa1
Victor Barragan <vbarraganp@unal.edu.co>
2022-06-03 22:39:22 -0500

    Https api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL : "http://localhost:3000/graphql",
+  API_URL : "//api.skillsly.io/graphql",
   SOCKET_URL: "http://localhost:3001",
   MAP_BOX_TOKEN: "pk.eyJ1IjoiZmVsaXBlcm9qYXMyNiIsImEiOiJja3g2cXJ3M2IycGplMnF0OWlmOHZ3ZG04In0.rjSKFBrW4c0nvWciQAru6g"
 };
